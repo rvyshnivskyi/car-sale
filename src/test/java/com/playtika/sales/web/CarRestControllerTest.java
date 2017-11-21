e8166a9ecbeea6654d1cc4a75684ed4e861a50cc
rvyshnivskyi <vyshnivskyi23@gmail.com>
2017-11-21 21:25:19 +0200

    Add test-cases of validatorsˆ

--- src/test/java/com/playtika/sales/web/CarRestControllerTest.java
+++ src/test/java/com/playtika/sales/web/CarRestControllerTest.java
@@ -3,6 +3,8 @@ package com.playtika.sales.web;
 import com.playtika.sales.domain.Car;
 import com.playtika.sales.domain.SaleDetails;
 import com.playtika.sales.service.CarService;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +12,9 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.Arrays;
 import java.util.Optional;
@@ -33,6 +37,9 @@ public class CarRestControllerTest {
 
     MockMvc mockMvc;
 
+    public CarRestControllerTest() throws JSONException {
+    }
+
     @Before
     public void setUp() throws Exception {
         mockMvc = MockMvcBuilders
@@ -47,7 +54,11 @@ public class CarRestControllerTest {
         returned.setId(1L);
         when(service.addCarForSale(generateCar(number), generateSaleDetails()))
                 .thenReturn(returned);
-        mockMvc.perform(post("/cars?price=0.1&firstName=firstName&phone=1234&lastName=lastName")
+        mockMvc.perform(post("/cars?")
+                .param("price", "0.1")
+                .param("firstName", "firstNAme")
+                .param("phone", "1234")
+                .param("lastName", "lastName")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(getCarJSON(number)))
                 .andExpect(status().isOk())
@@ -104,6 +115,76 @@ public class CarRestControllerTest {
                 .andExpect(status().isNoContent());
     }
 
+    @Test
+    public void carBrandNotBlankValidation() throws Exception {
+        MvcResult result = mockMvc.perform(post("/cars?")
+                .param("price", "0.1")
+                .param("firstName", "firstNAme")
+                .param("phone", "1234")
+                .param("lastName", "lastName")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(carJSON.put("brand", "").toString()))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        assertThat(result.getResolvedException().getClass(), typeCompatibleWith(MethodArgumentNotValidException.class));
+    }
+
+    @Test
+    public void carColorNotBlankValidation() throws Exception {
+        MvcResult result = mockMvc.perform(post("/cars?")
+                .param("price", "0.1")
+                .param("firstName", "firstNAme")
+                .param("phone", "1234")
+                .param("lastName", "lastName")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(carJSON.put("color", "").toString()))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        assertThat(result.getResolvedException().getClass(), typeCompatibleWith(MethodArgumentNotValidException.class));
+    }
+
+    @Test
+    public void carNumberNotBlankValidation() throws Exception {
+        MvcResult result = mockMvc.perform(post("/cars?")
+                .param("price", "0.1")
+                .param("firstName", "firstNAme")
+                .param("phone", "1234")
+                .param("lastName", "lastName")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(carJSON.put("number", "").toString()))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        assertThat(result.getResolvedException().getClass(), typeCompatibleWith(MethodArgumentNotValidException.class));
+    }
+
+    @Test
+    public void carAgeNotNegativeValidation() throws Exception {
+        MvcResult result = mockMvc.perform(post("/cars?")
+                .param("price", "0.1")
+                .param("firstName", "firstNAme")
+                .param("phone", "1234")
+                .param("lastName", "lastName")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(carJSON.put("age", "-1").toString()))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        assertThat(result.getResolvedException().getClass(), typeCompatibleWith(MethodArgumentNotValidException.class));
+    }
+
+    @Test
+    public void saleDetailsNotNegativeValidation() throws Exception {
+        MvcResult result = mockMvc.perform(post("/cars?")
+                .param("price", "-0.1")
+                .param("firstName", "firstNAme")
+                .param("phone", "1234")
+                .param("lastName", "lastName")
+                .contentType(APPLICATION_JSON_UTF8)
+                .content(carJSON.toString()))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+        assertThat(result.getResolvedException().getClass(), typeCompatibleWith(MethodArgumentNotValidException.class));
+    }
+
     private Car generateCar(String number) {
         Car car = new Car();
         car.setBrand("BMW");
@@ -122,7 +203,9 @@ public class CarRestControllerTest {
         return saleDetails;
     }
 
-    private String getCarJSON(final String number) {
-        return "{\"brand\":\"BMW\",\"color\":\"red\",\"age\":3,\"number\":\"" + number + "\"}";
+    private String getCarJSON(final String number) throws JSONException {
+        return carJSON.put("number", number).toString();
     }
+
+    JSONObject carJSON = new JSONObject("{\"brand\":\"BMW\",\"color\":\"red\",\"age\":3,\"number\":\"number\"}");
 }
